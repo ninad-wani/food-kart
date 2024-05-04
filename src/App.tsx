6b345d28b3c5ece917bd4c444bd372b82d23613b
Ninad Wani <iamninad02@gmail.com>
2024-05-04 15:37:04 +0530

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Items from './components/Items/Items'
 import MenuCard from './components/MenuCard/MenuCard'
-import Cart from './components/Cart/Cart'
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<MenuCard />} />
-        <Route path='/order' element={<Items />} />
-        <Route path='/cart' element={<Cart />} />
       </Routes>
     </BrowserRouter>
   )
--- src/components/Cart/Cart.tsx
+++
@@ -1,7 +0,0 @@
-const Cart = (props: any) => {
-  return (
-    <div>Cart</div>
-  )
-}
-
-export default Cart
